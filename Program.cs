c4b29d2a3df4d90ac44ef917ec8b78c90f0e22ff
phtn458 <phtn458@gmail.com>
2023-01-19 20:51:31 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿// using System;
+
+namespace practice;
+
+class Program
+{
+    public bool isPrime(int n)
+    {
+        if (n > 1)
+        {
+            for (var i = 2; i <= n / 2; i++)
+            {
+                if (n % i == 0) return false;
+            }
+        }
+        return true;
+    }
+
+    public int lcm(List<int> arr)
+    {
+        arr.Sort();
+        var range = new List<int> { };
+        for (var i = arr[0]; i <= arr[1]; i++)
+        {
+            range.Add(i);
+        }
+
+        int count = range[range.Count - 1];
+        while (!range.All(val => count % val == 0))
+        {
+            count++;
+        }
+        return count;
+    }
+
+    public static void Main(string[] args)
+    {
+        Program p = new Program();
+        var primes = new List<int> { };
+
+        for (var i = 2; i <= 3; i++)
+        {
+            if (p.isPrime(i))
+            {
+                primes.Add(i);
+            }
+        }
+        // foreach (var j in primes)
+        //     Console.WriteLine($"{j}");
+
+        var a = new List<int> { 13, 1 };
+        Console.WriteLine($"{p.lcm(a)}");
+    }
+}
